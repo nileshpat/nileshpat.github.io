7032463ab8813b1c12b813062360cb2b18bdc5d5
Nilesh <73364528+nileshpat@users.noreply.github.com>
2024-05-27 23:43:19 -0700

    Update Navbar.js
    Changed the alt text for the NKP Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react';
-//import './App.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a href="#" className="logo"><img src={`${process.env.PUBLIC_URL}/NKPLogo.JPG`} alt="C++" className="logo"/></a>
+        <a href="#" className="logo"><img src={`${process.env.PUBLIC_URL}/NKPLogo.JPG`} alt="NKP Logo" className="logo"/></a>
         <ul className="nav-links">
           <li><a href="#hero">Home</a></li>
           <li><a href="#about">About</a></li>
